44383e818ec351ff9f9155d8999aa62910503323
Samantaz Fox <coding@samantaz.fr>
2020-10-15 06:54:52 +0200

    Move list of supported devices to a PROGMEM array
    Also add some new devices that will be supported in the future

--- fusedoctor.ino
+++ fusedoctor.ino
@@ -20,15 +20,6 @@
 #define  LFUSE  0x646C
 #define  EFUSE  0x666E
 
-// Define ATTiny series signatures
-#define  ATTINY13   0x9007  // L: 0x6A, H: 0xFF             8 pin
-#define  ATTINY24   0x910B  // L: 0x62, H: 0xDF, E: 0xFF   14 pin
-#define  ATTINY25   0x9108  // L: 0x62, H: 0xDF, E: 0xFF    8 pin
-#define  ATTINY44   0x9207  // L: 0x62, H: 0xDF, E: 0xFFF  14 pin
-#define  ATTINY45   0x9206  // L: 0x62, H: 0xDF, E: 0xFF    8 pin
-#define  ATTINY84   0x930C  // L: 0x62, H: 0xDF, E: 0xFFF  14 pin
-#define  ATTINY85   0x930B  // L: 0x62, H: 0xDF, E: 0xFF    8 pin
-
 
 uint8_t shiftOut (uint8_t val1, uint8_t val2)
 {
