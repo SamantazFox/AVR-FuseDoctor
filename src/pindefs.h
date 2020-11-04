6f2e253126014c6c87b66c5b1454c1ea2e24eb9c
Samantaz Fox <coding@samantaz.fr>
2020-11-05 00:36:53 +0100

    Update pin definitions to match schematics

--- src/pindefs.h
+++ src/pindefs.h
@@ -8,11 +8,12 @@
 
 // Output to level shifter for !RESET from transistor
 #define  PORT_RST      A
-#define  PIN_RST       0
+#define  PIN_RST       1
 
 // Target VCC
 #define  PORT_VCC      A
-#define  PIN_VCC       1
+#define  PIN_VCC       0
+
 
 
 /*
@@ -20,55 +21,97 @@
 */
 
 // Target Clock Input
-#define  PORT_SCI      D
-#define  PIN_SCI       1
+#define  PORT_SCI      B
+#define  PIN_SCI       2
 
 // Target Data Output
-#define  PORT_SDO      D
-#define  PIN_SDO       0
+#define  PORT_SDO      B
+#define  PIN_SDO       3
 
 
 // Target Instruction Input
 #define  PORT_SII      B
-#define  PIN_SII       1
+#define  PIN_SII       0
 
 // Target Data Input
 #define  PORT_SDI      B
-#define  PIN_SDI       0
+#define  PIN_SDI       1
+
 
 
 /*
- * Pinouts for HV parallel programming
+ * Pinouts for HV parallel programming (control signals)
 */
 
-// Data pin(s)
-#define PORT_DATA      B
-
-
 // Target Clock Input
-#define  PORT_WR       D
-#define  PIN_WR        1
+#define PORT_XTAL1     D
+#define PIN_XTAL1      2
 
 // Target Data Output
-#define  PORT_RDY      D
-#define  PIN_RDY       0
+#define PORT_RDY       B
+#define PIN_RDY        3
 
 
-// Output enable
+// Write pulse (active low)
+#define PORT_WR        D
+#define PIN_WR         4
+
+// Output enable (active low)
 #define PORT_OE        D
-#define PIN_OE         3
+#define PIN_OE         5
 
 // Byte Select 1
 #define PORT_BS1       D
-#define PIN_BS1        4
+#define PIN_BS1        3
+
+// Byte Select 2
+#define PORT_BS2       D
+#define PIN_BS2        6
 
 // XTAL Action Bit 0
 #define PORT_XA0       D
-#define PIN_XA0        5
+#define PIN_XA0        1
 
 // XTAL Action Bit 1
 #define PORT_XA1       D
-#define PIN_XA1        6
+#define PIN_XA1        0
+
+
+
+/*
+ * Pinouts for HV parallel programming (data signals)
+*/
+
+// HC595 serial in (data out, same pin as SDI)
+#define PORT_HC_DATA   PORT_SDI
+#define PIN_HC_DATA    PIN_SDI
+
+// HC595 serial clock (same pin as SCI)
+#define PORT_HC_CLK    PORT_SCI
+#define PIN_HC_CLK     PIN_SCI
+
+// HC595 "store data" ("shift load"/"RCLK")
+#define PORT_HC_LOAD   B
+#define PIN_HC_LOAD    4
+
+// HC595 output enable
+#define PORT_HC_OE     B
+#define PIN_HC_OE      5
+
+
+
+/*
+ * Pinouts for status LEDS
+*/
+
+// Status good/polling LED (Green LED)
+#define PORT_LED_G     B
+#define PIN_LED_G      6
+
+// Error LED (Red)
+#define PORT_LED_R     B
+#define PIN_LED_R      7
+
 
 
 /*
@@ -114,6 +157,7 @@
 
 #define __pinMode(port, pin, state) \
 	{ if (state) sbi(DDR##port, pin) else cbi(DDR##port, pin); }
+
 
 
 #endif  /* !__PINDEFS_H__ */
